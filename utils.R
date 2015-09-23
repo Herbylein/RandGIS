16f06de622ef163f8233f90dd9ed9924714fee84
Rasilgon <Rasilgon@users.noreply.github.com>
2015-09-23 08:54:53 +0200

    Update utils.R

--- utils.R
+++ utils.R
@@ -4,3 +4,8 @@ sub("^([^.]*).*", "\\1", 'filename.extension')
 sub("^([^.]*).*", "\\1", 'filename.extension.and.more')
 [1] "filename"
 sub("^([^.]*).*", "\\1", 'filename without extension')
+
+# check if files ase empty
+info <- info = file.info(fileNames)
+fileNames.notempty = rownames(info[info$size != 0, ])
+fileNames.empty = rownames(info[info$size == 0, ])
